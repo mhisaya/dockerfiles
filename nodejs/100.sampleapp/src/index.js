e1fd6a7e28bbccbc7d6b9a0cf9a7b8fe2cd283c4
mhisaya <mhisaya@gmail.com>
2014-05-06 19:29:36 +0900

    add 'st' module

--- nodejs/100.sampleapp/src/index.js
+++ nodejs/100.sampleapp/src/index.js
@@ -1,14 +1,25 @@
+
 var express = require('express');
+var st = require('st');
 
 // Constants
 var DEFAULT_PORT = 8080;
 var PORT = process.env.PORT || DEFAULT_PORT;
 
+var mount = st({ path: '/src/angular/app', url: '/' })
+
 // App
 var app = express();
-app.get('/', function (req, res) {
+app.get('/rest', function (req, res) {
   res.send('Hello World\n');
 });
 
+app.get('/*', function (req, res) {
+  mount(req, res, function() {
+    res.end('This is not a static file.')
+  })
+});
+
 app.listen(PORT)
+
 console.log('Running on http://localhost:' + PORT);
